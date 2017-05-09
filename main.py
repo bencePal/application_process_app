1424a8a761256c6a0fc76799ad574538fd01773c
bencePal <paal.bence@gmail.com>
2017-05-09 12:07:34 +0200

    refactor error handling

--- main.py
+++ main.py
@@ -5,31 +5,28 @@ import ui
 def choose():
     inputs = ui.get_inputs(["Please enter a number: "], "")
     option = inputs[0]
-    try:
-        if option == "1":
-            queries.firstname_lastname_mentors()
-        elif option == "2":
-            queries.nickname_miskolc_mentors()
-        elif option == "3":
-            queries.carol_and_her_hat()
-        elif option == "4":
-            queries.another_girl_hat()
-        elif option == "5":
-            queries.add_new_applicant()
-        elif option == "6":
-            queries.change_phonenumber()
-        elif option == "7":
-            queries.del_arsiano_and_his_friend()
-        elif option == "8":
-            queries.all_data_mentors()
-        elif option == "9":
-            queries.all_data_applicants()
-        elif option == "0":
-            exit()
-        else:
-            raise KeyError("There is no such option.")
-    except Exception as err:
-                ui.print_error_message(err)
+    if option == "1":
+        queries.firstname_lastname_mentors()
+    elif option == "2":
+        queries.nickname_miskolc_mentors()
+    elif option == "3":
+        queries.carol_and_her_hat()
+    elif option == "4":
+        queries.another_girl_hat()
+    elif option == "5":
+        queries.add_new_applicant()
+    elif option == "6":
+        queries.change_phonenumber()
+    elif option == "7":
+        queries.del_arsiano_and_his_friend()
+    elif option == "8":
+        queries.all_data_mentors()
+    elif option == "9":
+        queries.all_data_applicants()
+    elif option == "0":
+        exit()
+    else:
+        raise KeyError("There is no such option.")
 
 
 def handle_menu():
@@ -48,11 +45,11 @@ def handle_menu():
 
 def main():
     while True:
-        handle_menu()
         try:
+            handle_menu()
             choose()
-        except KeyError as err:
-            ui.print_error_message(err)
+        except Exception as err:
+                ui.print_error_message(err)
 
 
 if __name__ == '__main__':
